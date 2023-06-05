bc294d243c7d4b89aa91a95009041f63a5a397b0
m0brown <mbrown1483@gmail.com>
2023-06-04 21:06:14 -0400

    Create new application API

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
+require("./app_api/database/db") //Database connection
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 
+//Reference new api router
+const apiRouter = require("./app_api/routes/index");
+
 var app = express();
-const hbs = require('hbs');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server','views'));
@@ -28,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
